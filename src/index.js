67d58f2d62a1c6c0b093040e043fef4b3728b6f2
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-28 19:29:14 +0530

    Implement Add todo-list feature

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const Project = (title) => {
 $('#projectModalCenter').on('shown.bs.modal', function () {
   $('#inputProjectTitle').focus()
 })
+
 function addProject() {
   const title = document.getElementById('inputProjectTitle').value;
   const project = Project(title);
@@ -36,3 +37,45 @@ const preventRefresh = (event) => {
 
 projectForm.addEventListener('submit', addProject);
 projectForm.addEventListener('submit', preventRefresh);
+
+// to-do list
+
+const toDoForm = document.getElementById('toDo-form');
+const toDoContainer = document.getElementById('toDo-container');
+const lists = [];
+
+$('#ToDoModalCenter').on('shown.bs.modal', function () {
+  $('#inputToDoTitle').focus()
+})
+
+const ToDo = (title, desc, prior, date, time, note) => {
+	return {title, desc, prior, date, time, note}
+}
+
+function addToDo(){
+	const title = document.getElementById('inputToDoTitle').value;
+	const desc = document.getElementById('inputToDoDesc').value;
+	const prior = document.getElementById('inputToDoPrior').value;
+	const date = document.getElementById('inputToDODate').value;
+	const time = document.getElementById('inputToDOTime').value;
+	const note = document.getElementById('inputToDONote').value;
+
+	const toDo = ToDo(title, desc, prior, date, time, note);
+	lists.push(toDo);
+	displayToDo(toDo);
+  	toDoForm.reset();
+  	$('#ToDoModalCenter').modal('toggle'); return false; 
+}
+
+function displayToDo(todo){
+const toDoSubmitBtn = document.getElementById('btn-toDo-add');
+    const toDoDiv = document.createElement('div');
+    toDoContainer.insertBefore(toDoDiv, toDoSubmitBtn);
+
+    const toDOTitle = document.createElement('p');
+    toDOTitle.innerText = todo.title;
+    toDoDiv.appendChild(toDOTitle);
+};
+
+toDoForm.addEventListener('submit', addToDo);
+toDoForm.addEventListener('submit', preventRefresh);
