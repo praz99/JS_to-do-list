acf954f15e3c123ed12ea4aaba5fc051c7574db7
praz <t.prazwal@gmail.com>
2020-10-28 11:55:13 +0100

    add autofocus in project input modal

--- src/index.js
+++ src/index.js
@@ -7,16 +7,21 @@ const Project = (title) => {
   return { title }
 };
 
+// autofocus modal input
+$('#projectModalCenter').on('shown.bs.modal', function () {
+  $('#inputProjectTitle').focus()
+})
 function addProject() {
   const title = document.getElementById('inputProjectTitle').value;
   const project = Project(title);
   projects.push(project);
   displayProject(project);
   projectForm.reset();
+  $('#projectModalCenter').modal('toggle'); return false; 
 }
 
 function displayProject(project) {
-  const projectSubmitBtn = document.getElementById('btn-project-submit');
+  const projectSubmitBtn = document.getElementById('btn-project-add');
     const projectDiv = document.createElement('div');
     projectContainer.insertBefore(projectDiv, projectSubmitBtn);
 
@@ -26,7 +31,7 @@ function displayProject(project) {
 };
 
 const preventRefresh = (event) => {
-  event.preventDefault();
+  event.preventDefault(); 
 };
 
 projectForm.addEventListener('submit', addProject);
