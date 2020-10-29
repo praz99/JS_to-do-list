443849339dab95a2b6a1674f627ee2cd5c94da10
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-29 21:19:59 +0530

    Separate the logic and dom and used factory function

--- src/index.js
+++ src/index.js
@@ -1,109 +1,126 @@
 import './css/style.css';
-const projectForm = document.getElementById('project-form');
-const projectContainer = document.getElementById('project-container');
-const projects = [];
-
-const Project = (title) => {
-  return { title }
-};
+import domController from './dom/domcontroller.js';
 
 // autofocus modal input
 $('#projectModalCenter').on('shown.bs.modal', function () {
   $('#inputProjectTitle').focus()
 })
-
-function addProject() {
-  const title = document.getElementById('inputProjectTitle').value;
-  const project = Project(title);
-  projects.push(project);
-  displayProject(project);
-  projectForm.reset();
-  $('#projectModalCenter').modal('toggle'); return false; 
-}
-
-function displayProject(project) {
-  const projectSubmitBtn = document.getElementById('btn-project-add');
-    const projectDiv = document.createElement('div');
-    projectContainer.insertBefore(projectDiv, projectSubmitBtn);
-
-    const projectTitle = document.createElement('p');
-    projectTitle.innerText = project.title;
-    projectDiv.appendChild(projectTitle);
-};
-
-const preventRefresh = (event) => {
-  event.preventDefault(); 
-};
-
-projectForm.addEventListener('submit', addProject);
-projectForm.addEventListener('submit', preventRefresh);
-
-// to-do list
-
-const toDoForm = document.getElementById('toDo-form');
-const toDoContainer = document.getElementById('toDo-container');
-const lists = [];
-
 $('#ToDoModalCenter').on('shown.bs.modal', function () {
   $('#inputToDoTitle').focus()
 })
 
-const ToDo = (title, desc, prior, date, time, note) => {
-	return {title, desc, prior, date, time, note}
-}
 
-function addToDo(){
-	const title = document.getElementById('inputToDoTitle').value;
-	const desc = document.getElementById('inputToDoDesc').value;
-	const prior = document.getElementById('inputToDoPrior').value;
-	const date = document.getElementById('inputToDODate').value;
-	const time = document.getElementById('inputToDOTime').value;
-	const note = document.getElementById('inputToDONote').value;
+// const preventRefresh = (event) => {
+//   event.preventDefault(); 
+// };
 
-	const toDo = ToDo(title, desc, prior, date, time, note);
-  lists.push(toDo);
-	displayToDo(toDo);
-  toDoForm.reset();
-  $('#ToDoModalCenter').modal('toggle'); return false;
-};
+const projectForm = document.getElementById('project-form');
+projectForm.addEventListener('submit', domController.domProjects);
 
-function displayToDo(todo){
-  const toDoSubmitBtn = document.getElementById('btn-toDo-add');
-  const toDoDiv = document.createElement('div');
-  toDoContainer.insertBefore(toDoDiv, toDoSubmitBtn);
+const toDoForm = document.getElementById('toDo-form');
+toDoForm.addEventListener('submit', domController.domToDos);
 
-  const toDOTitle = document.createElement('p');
-  toDOTitle.innerText = todo.title;
-  toDoDiv.appendChild(toDOTitle);
-  toDOTitle.addEventListener('click', showDetail(todo));
-  toDOTitle.addEventListener('click', preventRefresh);
-};
 
-toDoForm.addEventListener('submit', addToDo);
-toDoForm.addEventListener('submit', preventRefresh);
+// projectForm.addEventListener('submit', preventRefresh);
 
-// description
+// const projectForm = document.getElementById('project-form');
+// const projectContainer = document.getElementById('project-container');
+// const projects = [];
 
-function showDetail(todo) {
-  const detail = document.getElementById('desc');
-  const dTitle = document.createElement('div');
-  const dDesc = document.createElement('div');
-  const dPrior = document.createElement('div');
-  const dDate = document.createElement('div');
-  const dTime = document.createElement('div');
-  const dNote = document.createElement('div');
+// const Project = (title) => {
+//   return { title }
+// };
 
-  dTitle.innerText = todo.title;
-  dDesc.innerText = todo.desc;
-  dPrior.innerText = todo.prior;
-  dDate.innerText = todo.date;
-  dTime.innerText = todo.time;
-  dNote.innerText = todo.note;
+// function addProject() {
+//   const title = document.getElementById('inputProjectTitle').value;
+//   const project = Project(title);
+//   projects.push(project);
+//   displayProject(project);
+//   projectForm.reset();
+//   $('#projectModalCenter').modal('toggle'); return false; 
+// }
 
-  detail.appendChild(dTitle);
-  detail.appendChild(dDesc);
-  detail.appendChild(dPrior);
-  detail.appendChild(dDate);
-  detail.appendChild(dTime);
-  detail.appendChild(dNote);
-}
+// function displayProject(project) {
+//   const projectSubmitBtn = document.getElementById('btn-project-add');
+//     const projectDiv = document.createElement('div');
+//     projectContainer.insertBefore(projectDiv, projectSubmitBtn);
+
+//     const projectTitle = document.createElement('p');
+//     projectTitle.innerText = project.title;
+//     projectDiv.appendChild(projectTitle);
+// };
+
+// const preventRefresh = (event) => {
+//   event.preventDefault(); 
+// };
+
+// projectForm.addEventListener('submit', addProject);
+// projectForm.addEventListener('submit', preventRefresh);
+
+// // to-do list
+
+// const toDoForm = document.getElementById('toDo-form');
+// const toDoContainer = document.getElementById('toDo-container');
+// const lists = [];
+
+
+
+// const ToDo = (title, desc, prior, date, time, note) => {
+// 	return {title, desc, prior, date, time, note}
+// }
+
+// function addToDo(){
+// 	const title = document.getElementById('inputToDoTitle').value;
+// 	const desc = document.getElementById('inputToDoDesc').value;
+// 	const prior = document.getElementById('inputToDoPrior').value;
+// 	const date = document.getElementById('inputToDODate').value;
+// 	const time = document.getElementById('inputToDOTime').value;
+// 	const note = document.getElementById('inputToDONote').value;
+
+// 	const toDo = ToDo(title, desc, prior, date, time, note);
+//   lists.push(toDo);
+// 	displayToDo(toDo);
+//   toDoForm.reset();
+//   $('#ToDoModalCenter').modal('toggle'); return false;
+// };
+
+// function displayToDo(todo){
+//   const toDoSubmitBtn = document.getElementById('btn-toDo-add');
+//   const toDoDiv = document.createElement('div');
+//   toDoContainer.insertBefore(toDoDiv, toDoSubmitBtn);
+
+//   const toDOTitle = document.createElement('p');
+//   toDOTitle.innerText = todo.title;
+//   toDoDiv.appendChild(toDOTitle);
+//   toDOTitle.addEventListener('click', showDetail(todo));
+//   toDOTitle.addEventListener('click', preventRefresh);
+// };
+
+// toDoForm.addEventListener('submit', addToDo);
+// toDoForm.addEventListener('submit', preventRefresh);
+
+// // description
+
+// function showDetail(todo) {
+//   const detail = document.getElementById('desc');
+//   const dTitle = document.createElement('div');
+//   const dDesc = document.createElement('div');
+//   const dPrior = document.createElement('div');
+//   const dDate = document.createElement('div');
+//   const dTime = document.createElement('div');
+//   const dNote = document.createElement('div');
+
+//   dTitle.innerText = todo.title;
+//   dDesc.innerText = todo.desc;
+//   dPrior.innerText = todo.prior;
+//   dDate.innerText = todo.date;
+//   dTime.innerText = todo.time;
+//   dNote.innerText = todo.note;
+
+//   detail.appendChild(dTitle);
+//   detail.appendChild(dDesc);
+//   detail.appendChild(dPrior);
+//   detail.appendChild(dDate);
+//   detail.appendChild(dTime);
+//   detail.appendChild(dNote);
+// }
---
+++ src/logic/factory.js
@@ -0,0 +1,14 @@
+const Factory = (() => {
+	const projectFactory = (title) => {
+		let toDos = [];
+		return {title, toDos};
+	}
+
+	const toDoFactory = (title, desc, prior, date, time, note) => {
+		return {title, desc, prior, date, time, note};
+	}
+
+	return { projectFactory, toDoFactory};
+})();
+
+export default Factory;
