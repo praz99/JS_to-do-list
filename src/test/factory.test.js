59fe78e5ed44c2dae2a3b1eef21cd95cd8ab1ce6
praz <t.prazwal@gmail.com>
2020-11-12 16:45:13 +0100

    fix lint error

--- src/test/factory.test.js
+++ src/test/factory.test.js
@@ -1,47 +1,42 @@
-import {createProject, createTodo} from '../logic/factory';
+import { createProject, createTodo } from '../logic/factory';
 
-describe("Create a project", () => {
+describe('Create a project', () => {
+  const project = createProject('study');
 
-	const project = createProject("study");
+  test('return project name', () => {
+    expect(project.name).toEqual('study');
+  });
 
-test("return project name", () => {
-	expect(project.name).toEqual("study")
+  test('return project todos type', () => {
+    const project = createProject('study');
+    expect(typeof project.todos).toEqual('object');
+  });
 });
 
-test("return project todos type", () => {
-	const project = createProject("study");
-	expect(typeof project.todos).toEqual("object")
-});
+describe('Create a todo', () => {
+  const todo = createTodo('math', 'maths is about logic', 'low', '22-12-2020', '14:50', '10 chapters');
 
-});
+  test('return todo name', () => {
+    expect(todo.name).toEqual('math');
+  });
 
-describe("Create a todo", () => {
+  test('return todo description', () => {
+    expect(todo.desc).toEqual('maths is about logic');
+  });
 
-	const todo = createTodo("math", "maths is about logic", "low", "22-12-2020", "14:50", "10 chapters");
-	
-	test("return todo name", () => {
-	expect(todo.name).toEqual("math")
-	});
+  test('return todo date', () => {
+    expect(todo.date).toEqual('22-12-2020');
+  });
 
-	test("return todo description", () => {
-	expect(todo.desc).toEqual("maths is about logic")
-	});
+  test('return todo priority', () => {
+    expect(todo.prior).toEqual('low');
+  });
 
-	test("return todo date", () => {
-	expect(todo.date).toEqual("22-12-2020")
-	});
+  test('return todo time', () => {
+    expect(todo.time).toEqual('14:50');
+  });
 
-	test("return todo priority", () => {
-	expect(todo.prior).toEqual("low")
-	});
-
-	test("return todo time", () => {
-	expect(todo.time).toEqual("14:50")
-	});
-
-	test("return todo notes", () => {
-	expect(todo.note).toEqual("10 chapters")
-	});
-
-
+  test('return todo notes', () => {
+    expect(todo.note).toEqual('10 chapters');
+  });
 });
