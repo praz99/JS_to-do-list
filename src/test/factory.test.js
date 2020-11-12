550059994c0634b9bddbf82d7dec15d25c08b3ca
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-12 20:56:13 +0530

    Add more test with type of project todos

--- src/test/factory.test.js
+++ src/test/factory.test.js
@@ -1,13 +1,23 @@
 import {createProject, createTodo} from '../logic/factory';
 
-test("Create a project", () => {
+describe("Create a project", () => {
+
 	const project = createProject("study");
+
+test("return project name", () => {
 	expect(project.name).toEqual("study")
+});
+
+test("return project todos type", () => {
+	const project = createProject("study");
+	expect(typeof project.todos).toEqual("object")
+});
+
 });
 
 describe("Create a todo", () => {
 
-	const todo = createTodo("math", "maths is about logic", "low", "date", "time", "10 chapters");
+	const todo = createTodo("math", "maths is about logic", "low", "22-12-2020", "14:50", "10 chapters");
 	
 	test("return todo name", () => {
 	expect(todo.name).toEqual("math")
@@ -17,8 +27,16 @@ describe("Create a todo", () => {
 	expect(todo.desc).toEqual("maths is about logic")
 	});
 
+	test("return todo date", () => {
+	expect(todo.date).toEqual("22-12-2020")
+	});
+
 	test("return todo priority", () => {
 	expect(todo.prior).toEqual("low")
+	});
+
+	test("return todo time", () => {
+	expect(todo.time).toEqual("14:50")
 	});
 
 	test("return todo notes", () => {
